b01036e067a9676a34327c9bd8fa7738c530b4c0
ToffanTee <125197508+ToffanTee@users.noreply.github.com>
2025-06-23 00:30:31 +0100

    Update dbConnection.js

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -1,11 +1,24 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+// const envVariables = require("../constants/index");
+
+// const connectDatabase = async () => {
+//   await mongoose
+//     .connect(envVariables.DB_URI)
+//     .then(() => console.log("Database connected successfully"))
+//     .catch((error) => console.log("Database connection failed"));
+// };
+
+// module.exports = connectDatabase;
+
+const mongoose = require('mongoose');
 const envVariables = require("../constants/index");
 
-const connectDatabase = async () => {
-  await mongoose
-    .connect(envVariables.DB_URI)
-    .then(() => console.log("Database connected successfully"))
-    .catch((error) => console.log("Database connection failed"));
-};
-
-module.exports = connectDatabase;
+mongoose.connect(process.envVariables.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ Connected to MongoDB Atlas'))
+.catch((err) => {
+  console.error('❌ Database connection failed!');
+  console.error(err.message);  // <--- shows real reason
+});
