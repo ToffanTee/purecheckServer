741e2588b52ac146f59e3dd9a5f316a5f8d494ac
ToffanTee <125197508+ToffanTee@users.noreply.github.com>
2025-06-23 00:36:34 +0100

    Update dbConnection.js

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -1,26 +1,26 @@
-// const mongoose = require("mongoose");
-// const envVariables = require("../constants/index");
-
-// const connectDatabase = async () => {
-//   await mongoose
-//     .connect(envVariables.DB_URI)
-//     .then(() => console.log("Database connected successfully"))
-//     .catch((error) => console.log("Database connection failed"));
-// };
-
-// module.exports = connectDatabase;
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const envVariables = require("../constants/index");
 
-mongoose.connect(process.envVariables.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('✅ Connected to MongoDB Atlas'))
-.catch((err) => {
-  console.error('❌ Database connection failed!');
-  console.error(err.message);  // <--- shows real reason
-});
+const connectDatabase = async () => {
+  await mongoose
+    .connect(envVariables.DB_URI)
+    .then(() => console.log("Database connected successfully"))
+    .catch((error) => console.log("Database connection failed"));
+};
 
 module.exports = connectDatabase;
+
+// const mongoose = require('mongoose');
+// const envVariables = require("../constants/index");
+
+// mongoose.connect(process.envVariables.DB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('✅ Connected to MongoDB Atlas'))
+// .catch((err) => {
+//   console.error('❌ Database connection failed!');
+//   console.error(err.message);  // <--- shows real reason
+// });
+
+// module.exports = connectDatabase;
